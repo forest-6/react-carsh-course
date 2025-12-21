d39302032fa9117b2e2ce9167a0808b71841aae9
임승민 <seungmin3238@gmail.com>
2025-12-21 12:28:11 +0900

    동적인 값 출력하기

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,11 @@
+const names = ["Maximilian", "Menual"];
+
 function Post() {
+  // eslint-disable-next-line react-hooks/purity
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
   return (
     <div>
+      <p>{chosenName}</p>
       <p>This is a Post component.</p>
     </div>
   );
