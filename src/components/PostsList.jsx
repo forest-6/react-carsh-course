6b2969912aa6bd76151fcb8e1debda7431ab1c3a
임승민 <seungmin3238@gmail.com>
2025-12-21 14:35:39 +0900

    children 프로퍼티 추가

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostList() {
@@ -15,10 +16,12 @@ function PostList() {
   }
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      <Modal>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
+        />
+      </Modal>
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Maximilian" body="This is a Post2 component." />
