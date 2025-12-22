ee26dbb203f6819c5e3726135759529f150244ac
임승민 <seungmin3238@gmail.com>
2025-12-22 22:41:51 +0900

    게시글 추가 기능

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,33 +1,33 @@
-import { useState } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 import classes from "./PostsList.module.css";
+import { useState } from "react";
 
 function PostList({ onStopPosting, modalIsVisible }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
+  const [posts, setPosts] = useState([]);
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
   return (
     <>
       {modalIsVisible && (
         <Modal close={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-          />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
-        <Post author="Maximilian" body="This is a Post2 component." />
+        {posts.length > 0 &&
+          posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        {posts.length === 0 && (
+          <div style={{ textAlign: "center", color: "white" }}>
+            <h2>No posts yet</h2>
+            <p>Start adding some!</p>
+          </div>
+        )}
       </ul>
     </>
   );
